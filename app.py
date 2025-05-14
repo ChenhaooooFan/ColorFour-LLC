b9ebaaf73df0594781387b4e164e1ac75f6ca00b
Chenhao Fan <146764940+ChenhaooooFan@users.noreply.github.com>
2025-05-14 11:23:20 -0700

    app.py

--- app.py
+++ app.py
@@ -178,7 +178,7 @@ if st.button("🚀 点击生成分析报表") and this_week_file and last_week_f
 
      # 自动补货计算
     st.subheader("📦 补货建议表")
-    production_days = 6
+    production_days = 5
     shipping_days = 12
     safety_days = 10
     total_days = production_days + shipping_days + safety_days
